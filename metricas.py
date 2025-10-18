607c50303c565d7329cda3f9cc29070e0c901bc8
Care99 <cesar_rodas@outlook.com>
2025-10-18 11:59:16 -0300

    create_cluster_clusters function

--- metricas.py
+++ metricas.py
@@ -110,137 +110,138 @@ for year in years:
       for department in departments:
          folder(year,month,department)
 
-# Initialize an empty list to store the DataFrames
-dfs = []
-# Sort files
-sorted_files=sorted(os.listdir(processed_data_path))
-# Process each uploaded file
-for filename in sorted_files:
-    if filename.startswith('training_data'):
-        # Load and process the CSV file
-        print(filename)
-        df = load_and_process_csv(os.path.join(processed_data_path, filename))
-        # Append the DataFrame to the list
-        dfs.append(df)
+def create_cluster_clusters():
+  # Initialize an empty list to store the DataFrames
+  dfs = []
+  # Sort files
+  sorted_files=sorted(os.listdir(processed_data_path))
+  # Process each uploaded file
+  for filename in sorted_files:
+      if filename.startswith('training_data'):
+          # Load and process the CSV file
+          print(filename)
+          df = load_and_process_csv(os.path.join(processed_data_path, filename))
+          # Append the DataFrame to the list
+          dfs.append(df)
 
-# Concatenate all DataFrames along the rows
-combined_time_series = pd.concat(dfs, axis=0)
+  # Concatenate all DataFrames along the rows
+  combined_time_series = pd.concat(dfs, axis=0)
 
-# If you need the result as a NumPy array
-combined_ts_matrix = combined_time_series.values
-for i in range(len(combined_ts_matrix)):
-   for j in range(len(combined_ts_matrix[0])):
-      if(np.isnan(combined_ts_matrix[i][j])):
-         combined_ts_matrix[i][j]=(combined_ts_matrix[i][j-1]+combined_ts_matrix[i][j+1])/2
-
-# Print the resulting matrix
-print(len(combined_ts_matrix[0]))
-print(len(combined_ts_matrix))
-
-os.makedirs(resultado_funciones_path,exist_ok=True)
-distance_matrix_size = 96
-resultado_funciones_total = np.zeros((distance_matrix_size,distance_matrix_size))
-for k in range(4):
-  function_folder = os.path.join(resultado_funciones_path,bhattacharyya)
-  os.makedirs(function_folder,exist_ok=True)
-  distance_matrix_size = 24
-  resultado_funciones = np.zeros((distance_matrix_size,distance_matrix_size))
-  for i in range(int(len(combined_ts_matrix)/4)):
-    for j in range(i+1,int(len(combined_ts_matrix)/4)):
-      resultado_funciones[i,j] = bhattacharyya(combined_ts_matrix[(k*24)+i],combined_ts_matrix[(k*24)+j])
-      if(np.isinf(resultado_funciones[i,j])):
-          resultado_funciones[i,j] = 0
+  # If you need the result as a NumPy array
+  combined_ts_matrix = combined_time_series.values
   for i in range(len(combined_ts_matrix)):
-    for j in range(i+1,len(combined_ts_matrix)):
-      resultado_funciones_total[i,j] = bhattacharyya(combined_ts_matrix[i],combined_ts_matrix[j])
-      if(np.isinf(resultado_funciones_total[i,j])):
-          resultado_funciones_total[i,j] = 0
-  
-  # Perform hierarchical clustering
-  linkage_matrix = linkage(resultado_funciones, method='average')
-  # Get the headers for labeling
-  headers = df.columns
+    for j in range(len(combined_ts_matrix[0])):
+        if(np.isnan(combined_ts_matrix[i][j])):
+          combined_ts_matrix[i][j]=(combined_ts_matrix[i][j-1]+combined_ts_matrix[i][j+1])/2
 
-  # Plot the dendrogram with modified labels
-  plt.figure(figsize=(10, 10))
-  labelsX = departments
-  year = str(initial_year + k)
-  print(f'bhattacharyya_{year}')
-  dendrogram(linkage_matrix,labels=labelsX,orientation='top', color_threshold=0.7,leaf_rotation=90,leaf_font_size=7,)
+  # Print the resulting matrix
+  print(len(combined_ts_matrix[0]))
+  print(len(combined_ts_matrix))
 
-  # Create sub folders
-  csv_folder = os.path.join(function_folder,'csv')
-  os.makedirs(csv_folder, exist_ok=True)
-  svg_folder = os.path.join(function_folder,'svg')
-  os.makedirs(svg_folder, exist_ok=True)
+  os.makedirs(resultado_funciones_path,exist_ok=True)
+  distance_matrix_size = 96
+  resultado_funciones_total = np.zeros((distance_matrix_size,distance_matrix_size))
+  for k in range(4):
+    function_folder = os.path.join(resultado_funciones_path,bhattacharyya)
+    os.makedirs(function_folder,exist_ok=True)
+    distance_matrix_size = 24
+    resultado_funciones = np.zeros((distance_matrix_size,distance_matrix_size))
+    for i in range(int(len(combined_ts_matrix)/4)):
+      for j in range(i+1,int(len(combined_ts_matrix)/4)):
+        resultado_funciones[i,j] = bhattacharyya(combined_ts_matrix[(k*24)+i],combined_ts_matrix[(k*24)+j])
+        if(np.isinf(resultado_funciones[i,j])):
+            resultado_funciones[i,j] = 0
+    for i in range(len(combined_ts_matrix)):
+      for j in range(i+1,len(combined_ts_matrix)):
+        resultado_funciones_total[i,j] = bhattacharyya(combined_ts_matrix[i],combined_ts_matrix[j])
+        if(np.isinf(resultado_funciones_total[i,j])):
+            resultado_funciones_total[i,j] = 0
+    
+    # Perform hierarchical clustering
+    linkage_matrix = linkage(resultado_funciones, method='average')
+    # Get the headers for labeling
+    headers = df.columns
 
-  # Plot graph
-  plt.title(f'bhattacharyya-{year}')
-  plt.xlabel('Departamento')
-  plt.ylabel('Distancia')
+    # Plot the dendrogram with modified labels
+    plt.figure(figsize=(10, 10))
+    labelsX = departments
+    year = str(initial_year + k)
+    print(f'bhattacharyya_{year}')
+    dendrogram(linkage_matrix,labels=labelsX,orientation='top', color_threshold=0.7,leaf_rotation=90,leaf_font_size=7,)
 
-  # Save the plot
-  svg_file = f'bhattacharyya_{year}.svg'
-  svg_folder = os.path.join(svg_folder,svg_file)
-  plt.savefig(svg_folder)
+    # Create sub folders
+    csv_folder = os.path.join(function_folder,'csv')
+    os.makedirs(csv_folder, exist_ok=True)
+    svg_folder = os.path.join(function_folder,'svg')
+    os.makedirs(svg_folder, exist_ok=True)
 
-  matriz_distancia = DataFrame(resultado_funciones)
-  csv_file = f'bhattacharyya_{year}.csv'
-  #csv_folder = os.path.join(csv_folder,csv_file)
-  matriz_distancia.to_csv(os.path.join(csv_folder,csv_file))
-  
-  matriz_distancia = DataFrame(resultado_funciones_total + resultado_funciones_total.T - np.diag(np.diag(resultado_funciones_total)))
-  csv_file = f'bhattacharyya_all.csv'
-  matriz_distancia.to_csv(os.path.join(csv_folder,csv_file))
-  
-  #Close plot and finish
-  plt.clf()
-  plt.close()
-  
+    # Plot graph
+    plt.title(f'bhattacharyya-{year}')
+    plt.xlabel('Departamento')
+    plt.ylabel('Distancia')
 
+    # Save the plot
+    svg_file = f'bhattacharyya_{year}.svg'
+    svg_folder = os.path.join(svg_folder,svg_file)
+    plt.savefig(svg_folder)
 
-for folder in os.listdir(resultado_funciones_path):
-  current_folder = os.path.join(resultado_funciones_path,folder,'csv')
-  vector = []
-  files = os.listdir(current_folder)
-  for file in sorted(files):
-    if not (file[-7:].startswith('all')):
-      df = pd.read_csv(os.path.join(current_folder, file))
-      vector.append(df.to_numpy().flatten())
-
-  matriz_distancia = np.zeros((len(vector),len(vector)))
-  for i in range(len(matriz_distancia)):
-    for j in range(i+1,len(matriz_distancia)):
-      dist = distance.euclidean(vector[i], vector[j])
-      matriz_distancia[i,j] = dist
-      matriz_distancia[j,i] = dist
-  # Perform hierarchical clustering
-  condensed_distance_matrix = distance.squareform(matriz_distancia)
-  linkage_matrix = linkage(condensed_distance_matrix, method='average')
+    matriz_distancia = DataFrame(resultado_funciones)
+    csv_file = f'bhattacharyya_{year}.csv'
+    #csv_folder = os.path.join(csv_folder,csv_file)
+    matriz_distancia.to_csv(os.path.join(csv_folder,csv_file))
+    
+    matriz_distancia = DataFrame(resultado_funciones_total + resultado_funciones_total.T - np.diag(np.diag(resultado_funciones_total)))
+    csv_file = f'bhattacharyya_all.csv'
+    matriz_distancia.to_csv(os.path.join(csv_folder,csv_file))
+    
+    #Close plot and finish
+    plt.clf()
+    plt.close()
+    
 
 
-  # Get the headers for labeling
-  headers = df.columns
-  # Plot the dendrogram with modified labels
-  plt.figure(figsize=(10, 10))
-  labelsX = ["2020","2021","2022","2023"]
-  year = str(initial_year + k)
-  print(folder)
-  dendrogram(linkage_matrix,labels=labelsX,orientation='top', color_threshold=0.7,leaf_rotation=90,leaf_font_size=7,)
+  for folder in os.listdir(resultado_funciones_path):
+    current_folder = os.path.join(resultado_funciones_path,folder,'csv')
+    vector = []
+    files = os.listdir(current_folder)
+    for file in sorted(files):
+      if not (file[-7:].startswith('all')):
+        df = pd.read_csv(os.path.join(current_folder, file))
+        vector.append(df.to_numpy().flatten())
 
-  # Plot graph
-  plt.title(f'{folder}')
-  plt.xlabel('Year')
-  plt.ylabel('Distance')
+    matriz_distancia = np.zeros((len(vector),len(vector)))
+    for i in range(len(matriz_distancia)):
+      for j in range(i+1,len(matriz_distancia)):
+        dist = distance.euclidean(vector[i], vector[j])
+        matriz_distancia[i,j] = dist
+        matriz_distancia[j,i] = dist
+    # Perform hierarchical clustering
+    condensed_distance_matrix = distance.squareform(matriz_distancia)
+    linkage_matrix = linkage(condensed_distance_matrix, method='average')
 
-  # Save the plot
-  svg_file = f'{folder}.svg'
-  svg_folder = os.path.join(resultado_funciones_path,folder,svg_file)
-  csv_file = f'{folder}.csv'
-  csv_folder = os.path.join(resultado_funciones_path,folder,csv_file)
-  plt.savefig(svg_folder)
-  df_matriz_distancia = DataFrame(matriz_distancia)
-  df_matriz_distancia.to_csv(csv_folder)
-  #Close plot and finish
-  plt.clf()
-  plt.close()
+
+    # Get the headers for labeling
+    headers = df.columns
+    # Plot the dendrogram with modified labels
+    plt.figure(figsize=(10, 10))
+    labelsX = ["2020","2021","2022","2023"]
+    year = str(initial_year + k)
+    print(folder)
+    dendrogram(linkage_matrix,labels=labelsX,orientation='top', color_threshold=0.7,leaf_rotation=90,leaf_font_size=7,)
+
+    # Plot graph
+    plt.title(f'{folder}')
+    plt.xlabel('Year')
+    plt.ylabel('Distance')
+
+    # Save the plot
+    svg_file = f'{folder}.svg'
+    svg_folder = os.path.join(resultado_funciones_path,folder,svg_file)
+    csv_file = f'{folder}.csv'
+    csv_folder = os.path.join(resultado_funciones_path,folder,csv_file)
+    plt.savefig(svg_folder)
+    df_matriz_distancia = DataFrame(matriz_distancia)
+    df_matriz_distancia.to_csv(csv_folder)
+    #Close plot and finish
+    plt.clf()
+    plt.close()
