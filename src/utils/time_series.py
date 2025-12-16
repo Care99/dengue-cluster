256d1fe59e9dc37a1c2dd260f9baea7ec5a7da9d
Care99 <cesar_rodas@outlook.com>
2025-12-16 12:23:47 -0300

    Specify return type

--- src/utils/time_series.py
+++ src/utils/time_series.py
@@ -9,7 +9,7 @@ excel_file = os.path.join(csv_path,excel_name)
 unformatted_data = pd.read_csv(excel_file)
 data = unformatted_data.copy()
 data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d')
-def get_time_series_from_range(start_date:datetime,end_date:datetime,department:str):
+def get_time_series_from_range(start_date:datetime,end_date:datetime,department:str)->list:
     ts_dict = {}
     filtered_data = data[
         (data['disease'] == "DENGUE") 
@@ -19,18 +19,18 @@ def get_time_series_from_range(start_date:datetime,end_date:datetime,department:
     filtered_data['date'] = pd.to_datetime(filtered_data['date'], format='%Y-%m-%d')
     range_data = filtered_data[filtered_data['date'].between(start_date, end_date, inclusive='both')]
     ts_dict[department] = range_data.reset_index(drop=True)
-    return ts_dict[department]['incidence'].values
-def get_ts(year:str, week:str, department:str):
+    return ts_dict[department]['incidence'].values.flatten().tolist()
+def get_ts(year:str, week:str, department:str)->list:
     start_date = datetime.strptime(start_date_index[int(year.split('-')[0])-2019],'%Y-%m-%d') + timedelta(weeks=int(week))
     end_date = start_date + timedelta(weeks=11)
     return get_time_series_from_range(start_date,end_date,department)
 
-def get_historical_data(department:str):
+def get_historical_data(department:str)->list:
     start_date = datetime.strptime("2019-10-05",'%Y-%m-%d')
     end_date = datetime.strptime("2022-09-24",'%Y-%m-%d')
     return get_time_series_from_range(start_date,end_date,department)
 
-def get_2022_2023_data(department:str):
+def get_2022_2023_data(department:str)->list:
     start_date = datetime.strptime("2022-10-01",'%Y-%m-%d')
     end_date = datetime.strptime("2023-09-30",'%Y-%m-%d')
     return get_time_series_from_range(start_date,end_date,department)
